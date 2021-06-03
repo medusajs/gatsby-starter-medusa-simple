9e8b5774e5cf15b78f35627a55da2ae1189c3be4
Sebastian Rindom <skrindom@gmail.com>
2021-06-03 18:30:47 +0200

    working cart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,54 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react";
+
+import Medusa from "../services/medusa";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 const headingAccentStyles = {
   color: "#663399",
-}
+};
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
   borderRadius: 4,
-}
+};
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
-}
+};
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
   marginBottom: 30,
-}
+};
 
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
   verticalAlign: "5%",
-}
+};
 
 const docLinkStyle = {
   ...linkStyle,
   listStyleType: "none",
   marginBottom: 24,
-}
+};
 
 const descriptionStyle = {
   color: "#232129",
@@ -54,13 +56,13 @@ const descriptionStyle = {
   marginTop: 10,
   marginBottom: 0,
   lineHeight: 1.25,
-}
+};
 
 const docLink = {
   text: "Documentation",
   url: "https://www.gatsbyjs.com/docs/",
   color: "#8954A8",
-}
+};
 
 const badgeStyle = {
   color: "#fff",
@@ -76,7 +78,7 @@ const badgeStyle = {
   top: -2,
   marginLeft: 10,
   lineHeight: 1,
-}
+};
 
 // data
 const links = [
@@ -123,62 +125,116 @@ const links = [
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
     color: "#663399",
   },
-]
+];
+
+const contentStyles = {
+  display: "flex",
+  justifyContent: "space-between",
+};
+
+const cartStyles = {
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+};
+
+const productListStyles = {
+  flex: 1,
+};
 
 // markup
 const IndexPage = () => {
+  const [products, setProducts] = useState([]);
+
+  const [cart, setCart] = useState({});
+
+  useEffect(() => {
+    let cartId;
+    if (localStorage) {
+      cartId = localStorage.getItem("cart_id");
+    }
+
+    if (cartId) {
+      Medusa.cart.retrieve(cartId).then(({ data }) => {
+        setCart(data.cart);
+      });
+    } else {
+      Medusa.cart.create(cartId).then(({ data }) => {
+        setCart(data.cart);
+        if (localStorage) {
+          localStorage.setItem("cart_id", data.cart.id);
+        }
+      });
+    }
+
+    Medusa.products.list().then(({ data }) => {
+      setProducts(data.products);
+    });
+  }, []);
+
+  const handleAddToCart = (variantId) => {
+    if (cart && cart.id) {
+      Medusa.cart.lineItems
+        .create(cart.id, {
+          variant_id: variantId,
+          quantity: 1,
+        })
+        .then(({ data }) => {
+          setCart(data.cart);
+        });
+    }
+  };
+  const handleRemoveFromCart = (lineId) => {
+    if (cart && cart.id) {
+      Medusa.cart.lineItems.delete(cart.id, lineId).then(({ data }) => {
+        setCart(data.cart);
+      });
+    }
+  };
+
   return (
     <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+      <title>Medusa Home</title>
+      <h1 style={headingStyles}>Welcome to Medusa!</h1>
+      <p style={paragraphStyles}></p>
+      <div style={contentStyles}>
+        <div style={productListStyles}>
+          <h2>Products</h2>
+          {products.map((p) => {
+            return (
+              <div key={p.id}>
+                <strong>{p.title}</strong>
+                <p>{p.description}</p>
+                <button onClick={() => handleAddToCart(p.variants[0].id)}>
+                  Add to cart
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        <div style={cartStyles}>
+          <h2>Cart</h2>
 
-export default IndexPage
+          <div>
+            {cart.items?.map((i) => {
+              return (
+                <div key={i.id}>
+                  <strong>{i.title}</strong>
+                  <p>
+                    {i.quantity} x {(i.unit_price / 100).toFixed(2)}{" "}
+                    {cart.region.currency_code?.toUpperCase()}
+                  </p>
+                  <button onClick={() => handleRemoveFromCart(i.id)}>
+                    Remove
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default IndexPage;
